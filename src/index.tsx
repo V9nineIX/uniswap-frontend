1f82045b63ed274ca006fb54622f0932a55e4fd5
Wi9 <v9nineix@gmail.com>
2021-06-02 10:54:41 +0700

    Refactor code.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
 import 'inter-ui'
 import React, { StrictMode } from 'react'
-import { isMobile } from 'react-device-detect'
+// import { isMobile } from 'react-device-detect'
 import ReactDOM from 'react-dom'
-import ReactGA from 'react-ga'
+// import ReactGA from 'react-ga'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 import Blocklist from './components/Blocklist'
@@ -26,28 +26,28 @@ if (!!window.ethereum) {
   window.ethereum.autoRefreshOnNetworkChange = false
 }
 
-const GOOGLE_ANALYTICS_ID: string | undefined = process.env.REACT_APP_GOOGLE_ANALYTICS_ID
-if (typeof GOOGLE_ANALYTICS_ID === 'string') {
-  ReactGA.initialize(GOOGLE_ANALYTICS_ID, {
-    gaOptions: {
-      storage: 'none',
-      storeGac: false
-    }
-  })
-  ReactGA.set({
-    anonymizeIp: true,
-    customBrowserType: !isMobile ? 'desktop' : 'web3' in window || 'ethereum' in window ? 'mobileWeb3' : 'mobileRegular'
-  })
-} else {
-  ReactGA.initialize('test', { testMode: true, debug: true })
-}
+// const GOOGLE_ANALYTICS_ID: string | undefined = process.env.REACT_APP_GOOGLE_ANALYTICS_ID
+// if (typeof GOOGLE_ANALYTICS_ID === 'string') {
+//   ReactGA.initialize(GOOGLE_ANALYTICS_ID, {
+//     gaOptions: {
+//       storage: 'none',
+//       storeGac: false
+//     }
+//   })
+//   ReactGA.set({
+//     anonymizeIp: true,
+//     customBrowserType: !isMobile ? 'desktop' : 'web3' in window || 'ethereum' in window ? 'mobileWeb3' : 'mobileRegular'
+//   })
+// } else {
+//   ReactGA.initialize('test', { testMode: true, debug: true })
+// }
 
-window.addEventListener('error', error => {
-  ReactGA.exception({
-    description: `${error.message} @ ${error.filename}:${error.lineno}:${error.colno}`,
-    fatal: true
-  })
-})
+// window.addEventListener('error', error => {
+//   ReactGA.exception({
+//     description: `${error.message} @ ${error.filename}:${error.lineno}:${error.colno}`,
+//     fatal: true
+//   })
+// })
 
 function Updaters() {
   return (
